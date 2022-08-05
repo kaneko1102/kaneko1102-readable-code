5c51206f756d23b4b3d7ed8fbe850af750585df8
kaneko1102 <kaneko-takuya@ed.tmu.ac.jp>
2022-08-05 13:22:24 +0900

    spec5: WIP

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 int main(int argc, char** argv) {
     std::ifstream file(argv[1]);  // 読み込むファイルのパスを指定
     std::string str_line;
+    
+    int dic_id = 0;
 
     while (std::getline(file, str_line)) {  // 1行ずつ読み込む
-        std::cout << str_line << std::endl;
+        dic_id++;
+        std::cout << dic_id << ":" << str_line << std::endl;
     }
 
     return 0;
